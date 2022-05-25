d73fdb0ddfae1aa3d428abfea355a93c8f12f5ec
Player Ghost <sousaitalo12@gmail.com>
2022-05-25 18:35:30 -0300

    wip: addProduct function

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-contract Marketplace{
+contract Marketplace {
     address[16] public buyers;
 
-    function buy(uint productId) public returns (uint){
+    function buy(uint256 productId) public returns (uint256) {
         require(productId >= 0 && productId <= 15);
 
         buyers[productId] = msg.sender;
@@ -11,7 +11,13 @@ contract Marketplace{
         return productId;
     }
 
-    function getBuyers() public view returns (address[16] memory){
+    function getBuyers() public view returns (address[16] memory) {
         return buyers;
+    }
+
+    function addProduct() {
+        buyers.push(msg.sender);
+
+        return productId;
     }
 }
